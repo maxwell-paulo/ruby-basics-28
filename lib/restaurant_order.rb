6e4e205ea7b54c6f5c0dbb730939b12365f4032a
Maxwell da Silva <maxpaulo.mps@gmail.com>
2023-04-15 15:22:14 -0300

    final solution

--- lib/restaurant_order.rb
+++ lib/restaurant_order.rb
@@ -1,14 +1,18 @@
+MENU = {
+  "Hamburger"	=> 250,
+  "Cheese Burger"	=> 300,
+  "Veggie Burger"	=> 540,
+  "Vegan Burger"	=> 350,
+  "Sweet Potatoes"	=> 230,
+  "Salad"	=> 15,
+  "Iced Tea"	=> 70,
+  "Lemonade"	=> 90
+}
+
 def poor_calories_counter(burger, side, beverage)
-  menu = {
-    "Hamburger"	=> 250,
-    "Cheese Burger"	=> 300,
-    "Veggie Burger"	=> 540,
-    "Vegan Burger"	=> 350,
-    "Sweet Potatoes"	=> 230,
-    "Salad"	=> 15,
-    "Iced Tea"	=> 70,
-    "Lemonade"	=> 90
-  }
+
   # TODO: return number of calories for this restaurant order
-  menu[burger] + menu[side] + menu[beverage]
+  MENU[burger] + MENU[side] + MENU[beverage]
 end
+
+p poor_calories_counter("Hamburger", "Salad", "Lemonade")
